2afccd1a112f9b85ba450ade151d24acd7372887
matan922 <matanten@gmail.com>
2023-02-26 19:26:55 +0200

    deploy ready

--- frontend/app/src/globalVariables/endpoints.ts
+++ frontend/app/src/globalVariables/endpoints.ts
@@ -1,22 +1,22 @@
 
 // Game related endpoints
-export const getAllGames = "/games_list/games/";
-export const getSingleGame = "/games_list/game/";
-export const order = "/games_list/orders/"
-export const getGenres = "/games_list/genres/"
+export const getAllGames = "https://steam-like-serverback.onrender.com/games_list/games/";
+export const getSingleGame = "https://steam-like-serverback.onrender.com/games_list/game/";
+export const order = "https://steam-like-serverback.onrender.com/games_list/orders/"
+export const getGenres = "https://steam-like-serverback.onrender.com/games_list/genres/"
 
 
 // authentication related endpoints
-export const authLogin = "/auth/api/token/"
-export const authLogout = "/auth/api/token/refresh/"
-export const authRegister = "/auth/register/"
-export const authRegisterStaff = "/auth/register_staff/"
+export const authLogin = "https://steam-like-serverback.onrender.com/auth/api/token/"
+export const authLogout = "https://steam-like-serverback.onrender.com/auth/api/token/refresh/"
+export const authRegister = "https://steam-like-serverback.onrender.com/auth/register/"
+export const authRegisterStaff = "https://steam-like-serverback.onrender.com/auth/register_staff/"
 
 
 // community related endpoints
-export const profileGet = "/community/profile/"
-export const allProfilesGet = "/community/profiles/"
-export const steamGames = "/games_list/steamapi/"
+export const profileGet = "https://steam-like-serverback.onrender.com/community/profile/"
+export const allProfilesGet = "https://steam-like-serverback.onrender.com/community/profiles/"
+export const steamGames = "https://steam-like-serverback.onrender.com/games_list/steamapi/"
 
 
 // reviews endpoints
